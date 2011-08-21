9916e308ab79b6156b1845eecdc63f1cb3ebcca1
Yuhang Ge <abeyuhang@gmail.com>
2011-08-21 14:31:33 +0800

    css formater

---
+++ FormatCore/CSSFormater.h
@@ -0,0 +1,44 @@
+#pragma once
+#include<string>
+#include<stack>
+using namespace std;
+
+class CSSFormater
+{
+static const string INLINE_TAGS[];
+	int total_len;
+	int cur_idx;
+	int cur_intent_num;
+	char intent_char;
+	char intent_num;
+	string intent_str;
+
+	char cur_char;
+	char pre_char;
+	const char* content;
+	string output;
+	string doFormat();
+	
+	char getNextChar();
+	char skipSpace();
+
+
+	void formatStyleLine();
+
+	void formatDirect(const char until);
+	void formatStyleBlock();
+	void formatComment();
+	void formatLabel();
+
+	CSSFormater& append(const char c);
+	CSSFormater& append(const char* c);
+	CSSFormater& append(const string& c);
+	CSSFormater& appendNewLine(int intent_num);
+
+	bool isSpace(const char c);
+public:
+	static string format(const char* content);
+	CSSFormater(const char* content);
+	~CSSFormater(void);
+};
+
--- FormatCore/main.cpp
+++ FormatCore/main.cpp
@@ -1,14 +1,21 @@
 #include<iostream>
 #include<string>
-#include"HTMLFormater.h"
+#include"CSSFormater.h"
 using namespace std;
 
 
 
 int main(){
-	char text[]="<!doctype html><html>\n\n\n<head><link rel=\"stylesheet\"    href=\"a.css\"/><title>  Hello dsd  </title></head><body><p>hello formater   \n   love you alice</p></body></html>";
-	string out=HTMLFormater::format(text);
+	FILE* f=fopen("input.txt","r");
+	FILE* fo=fopen("output.txt","w");
+	char buf[10240];
+	int size=fread(buf,1,10240,f);
+	fclose(f);
+	buf[size]=0;
+	//char text[]="<!doctype html><html>\n\n\n<head><link rel=\"stylesheet\"    href=\"a.css\"/><title>  Hello dsd  </title></head><body><p>hello formater   \n   love you alice</p></body></html>";
+	string out=CSSFormater::format(buf);
 	cout<<out<<endl;
-
+	fwrite(out.c_str(),out.length(),1,fo);
+	fclose(fo);
 	system("pause");
 }
